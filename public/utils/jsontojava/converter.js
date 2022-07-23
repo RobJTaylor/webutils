8bc271074e413c78feef55b21f7f129df4e8e6ac
Rob Taylor <10993628+RobJTaylor@users.noreply.github.com>
2022-07-23 17:04:20 +0100

    Added JSON error handling with UI

--- public/utils/jsontojava/converter.js
+++ public/utils/jsontojava/converter.js
@@ -1,4 +1,26 @@
+function parseJson() {
+    try {
+        var json = JSON.parse($("#json").val());
+        $("#status").removeClass("alert-danger");
+        $("#status").addClass("alert-success");
+        $("#status").html("JSON successfully parsed!");
+        $("#status").fadeIn();
+    }
+    catch (e) {
+        $("#status").removeClass("alert-success");
+        $("#status").addClass("alert-danger");
+        $("#status").html("JSON cannot be parsed. Check your syntax.");
+        $("#status").fadeIn();
+    }
+}
+
 function convertJson() {
-    var json = JSON.parse($("#json").val());
-    console.log(json);
+    var json = "";
+    try {
+        json = JSON.parse($("#json").val());
+    }
+    catch (e) {
+        alert("JSON cannot be parsed. Check your syntax.");
+        return;
+    }
 }
